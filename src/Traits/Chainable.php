2579d0c9af880a997828ea052280c3e849d98549
skybluesofa <connect@skybluesofa.com>
2016-10-02 15:17:11 -0500

    Integrate Services

--- src/Traits/Chainable.php
+++ src/Traits/Chainable.php
@@ -11,26 +11,26 @@ trait Chainable
 
     public static function __callStatic($method, $parameters) {
         $instance = new static;
-        if ($this->methodCallable($instance, $method)) {
+        if ($instance->methodCallable($instance, $method)) {
             return call_user_func_array([$instance, $method], $parameters);
         }
     }
 
     private function methodCallable($object=null, $method=null) {
         if (is_null($object)) {
-            throw new Exception('Chainable subject has not been provided.');
+            throw new \Exception('Chainable subject has not been provided.');
         }
         if (!is_object($object)) {
-            throw new Exception('Chainable subject is not an object.');
+            throw new \Exception('Chainable subject is not an object.');
         }
 
         $class = get_class($object);
 
         if (is_null($method)) {
-            throw new Exception("Chainable method not provided for object {$class}.");
+            throw new \Exception("Chainable method not provided for object {$class}.");
         }
         if (!method_exists($object, $method)) {
-            throw new Exception("Chainable method does not exist on object {$class}.");
+            throw new \Exception("Chainable method does not exist on object {$class}.");
         }
 
         return true;
